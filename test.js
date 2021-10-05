d620ac4b550b7d8ce988d67c2043a52fd7cf97c7
GinBe <asdfwxwx@gmail.com>
2021-10-05 22:16:33 +0800

    Update test.js

--- test.js
+++ test.js
@@ -7,12 +7,37 @@ async function run() {
     try {
         await client.connect();
         // await listDatabases(client);
-        await createListing(client,{
-            name:"xiaoming",
-            summary:"hello xiaoming",
-            bedrooms:1,
-            bed_type:"Real Bed"
-        })
+
+        // await createListing(client,{
+        //     name:"xiaoming",
+        //     summary:"hello xiaoming",
+        //     bedrooms:1,
+        //     bed_type:"Real Bed"
+        // })
+
+        // await createmultiListing(client,[
+        //     {
+        //         name:"xiaoming1",
+        //         summary:"hello xiaoming1",
+        //         bedrooms:11,
+        //         bed_type:"Real Bed1"
+        //     },
+        //     {
+        //         name:"xiaoming2",
+        //         summary:"hello xiaoming2",
+        //         bedrooms:12,
+        //         bed_type:"Real Bed2"
+        //     },
+        //     {
+        //         name:"xiaoming3",
+        //         summary:"hello xiaoming3",
+        //         bedrooms:13,
+        //         bed_type:"Real Bed3"
+        //     }
+        // ])
+
+        await findOneListingByName(client,"xiaoming2");
+
     } catch(err) {
         console.error(err)
     } finally {
@@ -21,12 +46,8 @@ async function run() {
     }
 }
 run().catch(console.error);
-// console.log("nice")
-async function createListing(client,newListing){
-    const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertOne(newListing);
-    console.log(`New listing created with the followed id: ${result.insertedId}`)
-}
 
+//connect to the database
 async function listDatabases(client) {
     const databasesList = await client.db().admin().listDatabases();
     console.log("databases:")
@@ -34,4 +55,33 @@ async function listDatabases(client) {
         // console.log(`- ${db.name}`)
         console.log(`- ${db.name}`)
     })
+}
+
+// insertOne()
+async function createListing(client,newListing){
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertOne(newListing);
+    console.log(`New listing created with the followed id: ${result.insertedId}`)
+}
+
+// insertMany()
+async function createmultiListing(client,newListing){
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertMany(newListing);
+    console.log(`${result.count} lists have been inserted`)
+    console.log(result.insertedIds)
+}
+
+//findOne()
+async function findOneListingByName(client,nameOfListing){
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").findOne({name:nameOfListing});
+    if(result){
+        console.log(`Found a listing in the collection with the name ${nameOfListing}`)
+        console.log(result)
+    }else{
+        console.log(`could not find a listing in the collection with the name ${nameOfListing}`)
+    }
+}
+
+//find()
+async function findListingsByName(client,nameOfListing){
+    
 }
