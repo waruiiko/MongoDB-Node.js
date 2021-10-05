31a9a3381ea0da8cf747eaddc36190d6c00474a6
GinBe <asdfwxwx@gmail.com>
2021-10-05 21:23:46 +0800

    Update test.js

--- test.js
+++ test.js
@@ -6,7 +6,13 @@ const client = new MongoClient(uri);
 async function run() {
     try {
         await client.connect();
-        await listDatabases(client);
+        // await listDatabases(client);
+        await createListing(client,{
+            name:"xiaoming",
+            summary:"hello xiaoming",
+            bedrooms:1,
+            bed_type:"Real Bed"
+        })
     } catch(err) {
         console.error(err)
     } finally {
@@ -15,7 +21,11 @@ async function run() {
     }
 }
 run().catch(console.error);
-console.log("nice")
+// console.log("nice")
+async function createListing(client,newListing){
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertOne(newListing);
+    console.log(`New listing created with the followed id: ${result.insertedId}`)
+}
 
 async function listDatabases(client) {
     const databasesList = await client.db().admin().listDatabases();
